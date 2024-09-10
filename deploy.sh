1863e952cf4f0b081446d667c90cec8db84ce7ec
dim5x <dim5x@yahoo.com>
2024-09-10 23:56:03 +0300

    3

--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,47 @@
 #!/bin/bash
 
-# Запускать в папке проекта (/opt/testbot)
-#apt-get update
+# Параметры
+SERVICE_NAME="rss_bot"
+SCRIPT_PATH="/home/rss_bot/rss_bot.py"
+VENV_PATH="/home/rss_bot/ubuntu_env"
+
+# Запускать в папке проекта (/rss_bot)
 apt install -y python3-pip
 apt install -y python3-venv
 
 # создадим виртуальное окружение и установим в него зависимости согласно requirements.txt
 python3 -m venv ubuntu_env
-
 source ubuntu_env/bin/activate
-
 pip install -r requirements.txt
-
 deactivate
 
-# дадим право на исполнение скрипта запуска
-chmod +x run.sh
+# Создание файла сервиса
+cat <<EOL | sudo tee /etc/systemd/system/$SERVICE_NAME.service
+[Unit]
+Description=Rss_bot
+After=network.target
 
-# сконфигурируем сервис
-ln -s /home/dim5x/rss_bot/rss_bot.service /etc/systemd/system/rss_bot.service
+[Service]
+Type=simple
+User=$(whoami)
+WorkingDirectory=$(dirname $SCRIPT_PATH)
+ExecStart=$VENV_PATH/bin/python $SCRIPT_PATH
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOL
+
+#Права на сервис
 chmod 664 /etc/systemd/system/rss_bot.service
-systemctl daemon-reload
 
-# добавим в автозагрузку
-systemctl enable rss_bot
+# Перезагрузка конфигурации systemd
+sudo systemctl daemon-reload
 
-systemctl status rss_bot
+# Запуск и включение сервиса
+sudo systemctl start $SERVICE_NAME
+sudo systemctl enable $SERVICE_NAME
+
+echo "Сервис $SERVICE_NAME успешно создан и запущен."
+
+sudo systemctl status rss_bot
--- rss_bot.py
+++ rss_bot.py
@@ -1,10 +1,40 @@
+from difflib import SequenceMatcher
+import logging
+from logging.handlers import RotatingFileHandler
 import time
 from threading import Thread
+import xml.etree.ElementTree as ET
 
 from flask import Flask
 from newspaper import Article
 import requests
-import xml.etree.ElementTree as ET
+
+# Configure root logger
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# Create a rotating file handler
+handler = RotatingFileHandler('error.log', maxBytes=100000, backupCount=2, encoding='utf-8')  # 100000 bytes = 100 KB
+formatter = logging.Formatter('%(asctime)s \t %(name)s \t %(levelname)s \t %(message)s', datefmt='%d-%m-%Y %H:%M:%S')
+handler.setFormatter(formatter)
+
+# Add the handler to the root logger
+logger.addHandler(handler)
+
+
+
+def sim(a: str, b: str) -> float:
+    """
+    A function that calculates the similarity ratio between two input sequences.
+
+    Parameters:
+    a (any): The first input sequence.
+    b (any): The second input sequence.
+
+    Returns:
+    float: The similarity ratio between the two input sequences.
+    """
+    return SequenceMatcher(None, a, b).ratio()
 
 
 def parse_text(url: str) -> str:
@@ -20,36 +50,81 @@ def parse_text(url: str) -> str:
     article = Article(url, language='ru')  # Create Article object for the given URL
     article.download()  # Download the article content
     article.parse()  # Parse the article
-    text = article.text.split('\n')  # Split text into lines
-    text = '\n'.join(s for s in text[1:] if s)  # Join non-empty lines with newline
-    return text
+
+    # If no text is extracted, return an empty string
+    if not article.text:
+        return ''
+
+    # Clean up the text content
+    article_text = article.text.replace('\n\n', '\n')
+    article_text = article_text.split('\n')[1:]  # Remove the title
+
+    # Add period at the end of each line if not present
+    article_text = [line + '.' if line and not line.endswith('.') else line for line in article_text]
+
+    ind = 50
+    try:
+        # Check similarity between the first two lines and remove if similar
+        similarity = sim(article_text[0], article_text[1])
+        if similarity >= 0.3:
+            article_text = article_text[1:]
+
+        # Find and remove the last line containing 'Ранее'
+        ind = max([i for i, line in enumerate(article_text) if 'Ранее' in line])
+    except Exception:
+        pass
+
+    # Join non-empty lines with newline
+    article_text = '\n'.join(line for line in article_text[:ind] if line)
+
+    return article_text
 
 
-def parse_lenta_rss():
+def parse_lenta_rss() -> None:
+    """
+    Function to parse the RSS feed from Lenta.ru
+    """
     while True:
         url = 'https://lenta.ru/rss'
-        response = requests.get(url)
-        with open('lenta.xml', 'wb') as file:
-            file.write(response.content)
+        # Connect
+        try:
+            response = requests.get(url)
+            logging.info('Connected to Lenta!')
+            if response.status_code != 200:
+                raise Exception('Something went wrong with request to Lenta!')
+            with open('lenta.xml', 'wb') as file:
+                file.write(response.content)
+        except Exception as e:
+            print(e)
+            logging.error(e)
+            time.sleep(60 * 60)
+            continue
 
-        tree = ET.parse('lenta.xml')
-        root = tree.getroot()
+        tree = ET.parse('lenta.xml')  # Parse the XML file
+        root = tree.getroot()  # Get the root of the XML tree
 
-        for item in root.iter('item'):
-            category = item.find('category').text
-            if category in ('Путешествия', 'Спорт'):
+        # Parse
+        for ind, item in enumerate(root.iter('item'), start=1):
+            title = item.find('title').text
+            print(ind, '-', title)
+
+            try:
+                category = item.find('category').text
+                if category not in ('Путешествия', 'Спорт'):
+                    for element in item:
+                        if element.tag == 'link':
+                            link = element.text
+                        if element.tag in ('author', 'category', 'guid'):
+                            item.remove(element)
+                        if element.tag == 'description' and len(element.text) < 10:
+                            element.text = parse_text(link)  # Parse and update description if condition is met
+                tree.write('output.xml', encoding='utf-8')  # Write the updated XML tree to a new file
+            except Exception as e:
+                logging.error(e, title)
                 continue
-            else:
-                for element in item:
-                    if element.tag == 'link':
-                        link = element.text
-                    if element.tag in ('author', 'category', 'guid'):
-                        item.remove(element)
-                    if element.tag == 'description' and len(element.text) < 10:
-                        element.text = parse_text(link)
-            tree.write('output.xml', encoding='utf-8')
+
         print('RSS parsed successfully!')
-        time.sleep(60 * 60)
+        time.sleep(60 * 60)  # Sleep for an hour before parsing next iteration
 
 
 thread = Thread(target=parse_lenta_rss)
@@ -59,8 +134,12 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def hello_world() -> str:
+    """
+    A function that returns a message based on whether a thread is alive.
+    """
+    message = '&#128994;' if thread.is_alive() else '&#128308;'
+    return message
 
 
 @app.route('/rss')
